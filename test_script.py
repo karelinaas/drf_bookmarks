73d4e01883e5eb82592460ad09e5e8fdfaf78bff
karelinaas <karelinaas95@gmail.com>
2023-01-29 05:05:24 +0300

    changed test script for webinar (feature1)

--- test_script.py
+++ test_script.py
@@ -1,8 +1,12 @@
-def chunk(list_, size):
+def chunk_function(list_, size):
     return [list_[i:i+size] for i in range(0, len(list_), size)]
 
 
 lst = [i for i in range(25)]
 
 
-chunk(lst, 7)
+chunk_function(lst, 7)
+
+smth = False
+if not smth:
+    print('kek')
