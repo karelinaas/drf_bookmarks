d947839c09c6cfce837da8da3b9a7a770444d9ff
karelinaas <karelinaas95@gmail.com>
2023-01-29 05:04:05 +0300

    changed test script for webinar (feature2)

--- test_script.py
+++ test_script.py
@@ -1,8 +1,12 @@
-def chunk(list_, size):
+def chunk_maker(list_, size):
     return [list_[i:i+size] for i in range(0, len(list_), size)]
 
 
 lst = [i for i in range(25)]
 
 
-chunk(lst, 7)
+chunk_maker(lst, 7)
+
+var = True
+if var:
+    print('lol')
