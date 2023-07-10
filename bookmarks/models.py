4b68ff3392a2648df7d75a7af208022b4621dd3c
karelinaas <karelinaas95@gmail.com>
2023-07-10 18:55:11 +0300

    new model & fields

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,18 +1,28 @@
-from django.db.models import CharField, DateTimeField, Model, Q, UniqueConstraint, URLField
+from django.db import models
 from django.utils.timezone import now
 
 
-class Bookmark(Model):
-    time_created = DateTimeField(auto_now_add=True)
-    time_deleted = DateTimeField(null=True)
-    favicon = URLField(null=True)
-    url = URLField(verbose_name='URL')
-    title = CharField(max_length=255)
-    description = CharField(null=True, max_length=255)
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    order = models.IntegerField()
+
+
+class Bookmark(models.Model):
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_deleted = models.DateTimeField(null=True)
+    favicon = models.URLField(null=True)
+    url = models.URLField(verbose_name='URL')
+    title = models.CharField(max_length=255)
+    description = models.CharField(null=True, max_length=255)
+    group = models.ForeignKey(to=Group, null=True, on_delete=models.SET_NULL)
 
     class Meta:
         constraints = [
-            UniqueConstraint(fields=['url'], condition=Q(time_deleted__isnull=True), name='URL_UNIQUE_IF_NOT_DELETED')
+            models.UniqueConstraint(
+                fields=['url'],
+                condition=models.Q(time_deleted__isnull=True),
+                name='URL_UNIQUE_IF_NOT_DELETED',
+            )
         ]
 
     def delete(self, using=None, keep_parents=False):
